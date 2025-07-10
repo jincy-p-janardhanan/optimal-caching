634caac7cd2f54f77250330941a73d8b90702e63
Jincy P Janardhanan <jincypjanardhanan@gmail.com>
2025-07-10 12:43:24 +0530

    full graph

--- optimal_caching.py
+++ optimal_caching.py
@@ -6,16 +6,15 @@ def fixed_n(n=5):
 def get_n(utility_vec, p, f_0, C, plot=True):
     n = 2
     b = f_0 - C['C1']
-    G = [utility_vec[1]]
-    h = [(b + p * utility_vec[1] - C['C1'])/(1 + (1/p))]
+    G = [utility_vec[1]]   #starting from age 1 since utility_vec[0] = f_0
+    h = [(b + p * utility_vec[1] - C['C2'])/(1 + (1/p))]
     n_opt = 0
     
     while(n < len(utility_vec)):
         G.append(G[-1] + utility_vec[n])
         h.append( (b + p * G[-1] - n * C['C2'])/(n + (1/p)) )
-        if h[-1] < h[-2]:
+        if h[-1] < h[-2] and n_opt == 0:
             n_opt = n-1
-            break
         n += 1
     
     # print(h)
